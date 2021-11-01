40ab23d83f964b9c6504554a1614333d023231a0
juliebdick <juliebdick@gmail.com>
2021-11-01 15:19:21 -0500

    Client class

---
+++ ArkansasAssetBuilders/Models/Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ArkansasAssetBuilders.Models
+{
+    public class Client
+    { 
+        public int ClientID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DoB { get; set; }
+        public int Last4SS { get; set; }
+    }
+}
