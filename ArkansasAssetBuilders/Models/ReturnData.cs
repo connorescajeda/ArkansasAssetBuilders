79d9e7d08c4cfa20fb5e5c65863a45fe2b5725ac
juliebdick <juliebdick@gmail.com>
2021-11-01 15:28:43 -0500

    ReturnData class

---
+++ ArkansasAssetBuilders/Models/ReturnData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ArkansasAssetBuilders.Models
+{
+    public class ReturnData
+    {
+        public int ClientID { get; set; }
+        public int TaxYear { get; set; }
+        public bool FederalReturn { get; set; }
+        public int TotalRefund { get; set; }
+        public int EITC { get; set; }
+        public int CTC { get; set; }
+        public int Dependents { get; set; }
+        public int SurveyScore { get; set; }
+    }
+}
