b5765f847f2eb35df12fc04d4ea89d51749211bc
Garrett <colliergc@hendrix.edu>
2021-11-08 12:53:54 -0600

    Update Home Page and add Upload file piece

--- ArkansasAssetBuilders/Pages/Index.cshtml.cs
+++ ArkansasAssetBuilders/Pages/Index.cshtml.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.IO;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace ArkansasAssetBuilders.Pages
@@ -11,15 +11,35 @@ namespace ArkansasAssetBuilders.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IHostEnvironment _environment;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        [BindProperty]
+        public IFormFile UploadedFile { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, IHostEnvironment environment)
         {
             _logger = logger;
+            _environment = environment;
         }
 
         public void OnGet()
         {
+        }
 
+        public async Task OnPostAsync()
+        {
+            if (UploadedFile == null || UploadedFile.Length == 0)
+            {
+                return;
+            }
+
+            _logger.LogInformation($"Uploading {UploadedFile.FileName}.");
+            string targetFileName = $"{_environment.ContentRootPath}/{UploadedFile.FileName}";
+
+            using (var stream = new FileStream(targetFileName, FileMode.Create))
+            {
+                await UploadedFile.CopyToAsync(stream);
+            }
         }
     }
 }
